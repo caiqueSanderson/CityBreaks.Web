984b49be0bc63860bcfb9932815f58670f19a333
sanderson <caique.borges@al.infnet.edu.br>
2025-06-05 18:35:43 -0300

    Adicionar arquivos de projeto.

---
+++ Data/CityBreaksContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.Metrics;
+using CityBreaks.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityBreaks.Web.Data
+{
+    public class CityBreaksContext : DbContext
+    {
+        public CityBreaksContext(DbContextOptions<CityBreaksContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
