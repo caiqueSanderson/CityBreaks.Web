ebcb364b44248d188008307cc88c315a5374d3a4
sanderson <caique.borges@al.infnet.edu.br>
2025-06-05 19:13:42 -0300

    Exercicio 3 concluido

--- Models/Country.cs
+++ Models/Country.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string CountryCode { get; set; } = string.Empty;
         public string CountryName { get; set; } = string.Empty;
+        public List<City> Cities { get; set; } = new();
     }
 }
